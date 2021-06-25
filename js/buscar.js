5edc049c69c50ed1775e65ded16675bdaedf4bb5
BryanMurga <20203tn041@utez.edu.mx>
2021-06-25 15:46:42 -0500

    Examen Unidad II Bryan Mitchel Murga Arcos

---
+++ js/buscar.js
@@ -0,0 +1,4 @@
+const formBuscar = () =>{
+    let search = document.getElementById("search").value;
+    alert(`La busqueda con nombre ${search} no se encuentra, por lo cual intente mas tarde`);
+}
